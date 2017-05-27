72077a4bec56f4886d409b9c28d9c182771b6f4d
Filip Kucharczyk <filip.kucharczyk@ihsmarkit.com>
2017-05-27 13:11:30 +0200

    formatting

--- asterTake2/Mover.cs
+++ asterTake2/Mover.cs
@@ -14,10 +14,10 @@ namespace asterTake2
             var asteroidMinX = 0 - asteroid.Radius;
             var asteroidMaxY = mapHeight + asteroid.Radius;
             var asteroidMinY = 0 - asteroid.Radius;
-            if (asteroid.Position.X > asteroidMaxX)  { asteroid.Position.X = asteroidMinX; }
-            else if (asteroid.Position.X < asteroidMinX)         { asteroid.Position.X = asteroidMaxX; }
+            if      (asteroid.Position.X > asteroidMaxX) { asteroid.Position.X = asteroidMinX; }
+            else if (asteroid.Position.X < asteroidMinX) { asteroid.Position.X = asteroidMaxX; }
             if      (asteroid.Position.Y > asteroidMaxY) { asteroid.Position.Y = asteroidMinY; }
-            else if (asteroid.Position.Y < asteroidMinY)         { asteroid.Position.Y = asteroidMaxY; }
+            else if (asteroid.Position.Y < asteroidMinY) { asteroid.Position.Y = asteroidMaxY; }
             asteroid.Position = asteroid.Position.Offset(offset);
         }
     }
