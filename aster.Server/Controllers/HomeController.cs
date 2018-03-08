a7e6b3d440cca78ab877489998a5dee867ef9769
Filip Kucharczyk <fku-ext@danskecommodities.com>
2018-03-08 22:24:00 +0100

    wip - handling score at server

--- aster.Server/Controllers/HomeController.cs
+++ aster.Server/Controllers/HomeController.cs
@@ -8,11 +8,21 @@ namespace aster.Server.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<int> _scores = new List<int>();
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            ViewBag.Scores = _scores;
             return View();
+        }
+
+        public ActionResult AddScore(int score)
+        {
+            _scores.Add(score);
+            ViewBag.Title = "Home Page";
+            ViewBag.Scores = _scores;
+            return View("Index");
         }
     }
 }
